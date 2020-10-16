e2035e3bb913eb03c4220dabed8e933d6ccfc93d
Edwin García <spark.com.co@gmail.com>
2020-10-16 14:57:34 -0500

    Operaciones con strings y el comando update

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, new_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name, new_name)
+    else:
+        print('This clients does not exist')
+
+
 def _add_comma():
     global clients
     clients += ','
@@ -26,19 +35,30 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    return input('What\'s the client name?: ')
 
 
 if __name__ == "__main__":
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What\'s the client name?: ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        new_name = input('What is the new name?: ')
+        update_client(client_name, new_name)
+        list_clients()
     else:
         print('Invalid command!')
